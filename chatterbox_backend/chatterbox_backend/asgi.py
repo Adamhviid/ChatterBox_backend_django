4d400983b194f7378c02b2403f6427b7b68c2b07
Adam <adam5596@stud.kea.dk>
2024-09-02 14:09:27 +0200

    fiexed asgi

--- chatterbox_backend/chatterbox_backend/asgi.py
+++ chatterbox_backend/chatterbox_backend/asgi.py
@@ -1,23 +1,14 @@
-"""
-ASGI config for chatterbox_backend project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chat.routing import websocket_urlpatterns
+import chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatterbox_backend.settings")
 
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+        "websocket": AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns)),
     }
 )
